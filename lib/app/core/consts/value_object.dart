aee19c3e788644baab30eddde9a3e6b89666451b
krishpranav <krisna.pranav@gmail.com>
2023-04-28 17:29:35 +0530

    consts: value_object

--- lib/app/core/consts/value_object.dart
+++ lib/app/core/consts/value_object.dart
@@ -5,20 +5,33 @@ import 'package:now_btc_wallet/app/core/failures/failures.dart';
 abstract class ValueObject<T> {
   Either<BdkFailure<T>, T> get value;
 
+  // get or crash
   T getOrCrash() {
     return value.fold(
         (left) => throw UnexpectedValueError(left), (right) => right);
   }
 
+  // check is valid
   bool isValid() => value.isRight();
 
+  // get failure unit
   Either<BdkFailure<dynamic>, Unit> get failureOrUnit {
     return value.fold((l) => Left(l), (r) => const Right(unit));
   }
 
+  // operator
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is ValueObject<T> && other.value == value;
+  }
+
+  // hash code
   @override
   int get hashCode => value.hashCode;
 
+  // to string
   @override
   String toString() => '(value: $value)';
 }
