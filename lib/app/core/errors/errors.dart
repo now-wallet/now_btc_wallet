efd1e231749c6bd7e5bd2edcab40c2ae3c020ecc
krishpranav <krisna.pranav@gmail.com>
2023-04-28 17:18:30 +0530

    core: errors

---
+++ lib/app/core/errors/errors.dart
@@ -0,0 +1,15 @@
+import 'package:now_btc_wallet/app/core/failures/failures.dart';
+
+class UnexpectedValueError extends Error {
+  final BdkFailure valueFailure;
+
+  UnexpectedValueError(this.valueFailure);
+
+  @override
+  String toString() {
+    const errorExplanation = 'Encountered an unexpected ValueError. Please try'
+        ' again later ';
+    return Error.safeToString(
+        '$errorExplanation The Failure was:$valueFailure}');
+  }
+}
